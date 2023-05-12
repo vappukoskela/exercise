e471f93b8af0dd13055246a0a2619cd83c62b6fd
ossitammi <ossi.tammi@gmail.com>
2023-05-12 15:26:53 +0300

    Initial page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,36 @@
-import { Box } from '@mui/material';
+import { Map } from "./Map";
+import { Box, Grid, Typography } from "@mui/material";
 
 export function App() {
   return (
-    <Box>
-      <h1>TODO: APP</h1>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        justifyContent: "space-between",
+      }}
+    >
+      <Box
+        sx={{ backgroundColor: "#00B2A0", height: "40px", width: "100%" }}
+      ></Box>
+      <Box sx={{ width: "100%", height: "100%" }}></Box>
+      <Box
+        sx={{
+          backgroundColor: "#00B2A0",
+          height: "40px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {" "}
+        <Typography sx={{ color: "white" }} variant="overline">
+          Powered by Ubigu Oy
+        </Typography>
+      </Box>
     </Box>
   );
 }
