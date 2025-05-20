f6e0bb96d8ba55aa60c8cbac9309b6d14ffccd54
vappukoskela <vappu.koskela@gmail.com>
2025-05-20 21:22:29 +0300

    removed comments

--- client/src/HedgehogInfo.tsx
+++ client/src/HedgehogInfo.tsx
@@ -68,17 +68,6 @@ export function HedgehogInfo({ hedgehogId }: Props) {
             siili
           </Typography>
         )}
-        {/* <Typography>
-        TODO: Esitä tässä komponentissa haluamallasi tavalla yksittäisen, tällä
-        hetkellä valittuna olevan, siilin tiedot. Siili valitaan
-        vasemmanpuoleisesta listauksesta. Kartalla esitetään valitun siilin
-        sijainti karttamerkin avulla.
-      </Typography>
-      <br />
-      <Typography>
-        Komponentille välitetään React propertynä yksittäisen siilin ID, jonka
-        muuttuessa ko. siilin tiedot haetaan rajapinnalta.
-      </Typography> */}
         <br />
       </Box>
     </Paper>
