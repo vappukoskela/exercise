c914f8ca246228dc01202a6e17fd69b926893976
vappukoskela <vappu.koskela@gmail.com>
2025-05-20 08:23:55 +0300

    Added necessary backend functionality for adding and fetching hedgehogs

--- server/src/application/hedgehog.ts
+++ server/src/application/hedgehog.ts
@@ -3,10 +3,14 @@ import { logger } from "@server/logging";
 import { hedgehogSchema } from "@shared/hedgehog";
 import { sql } from "slonik";
 
+// When getting hedgehogs, note that location is stored as a ST_Point
+// This should be converted to latitude and longitude before returning
 export async function getAllHedgehogs() {
   try {
     const hedgehogs = await getPool().any(
-      sql.type(hedgehogSchema)`SELECT id FROM hedgehog`
+      sql.type(hedgehogSchema)`
+      SELECT id, name, gender, age, ST_Y(location) as latitude, ST_X(location) as longitude
+      FROM hedgehog`
     );
 
     return hedgehogs;
@@ -14,6 +18,37 @@ export async function getAllHedgehogs() {
     logger.error(error);
   }
 }
+
+export async function getHedgehog(id: number) {
+  try {
+    const hedgehog = await getPool().maybeOne(
+      sql.type(hedgehogSchema)`
+      SELECT id, name, gender, age, ST_Y(location) as latitude, ST_X(location) as longitude
+      FROM hedgehog
+      WHERE id = ${id}`
+    );
+
+    return hedgehog;
+  } catch (error) {
+    logger.error(error);
+  }
+}
+
+// Add new hedgehog. Using a spatial type point to store the location
+// This is to retain the possibility to use PostGIS functionality in the future 
+export async function addHedgehog(name: string, age: number, gender: string, latitude: number, longitude: number) {
+  try {
+    const newHedgehog = await getPool().one(
+      sql.type(hedgehogSchema)`INSERT INTO hedgehog (name, age, gender, location) VALUES (
+        ${name}, ${age}, ${gender}, ST_MakePoint(${longitude}, ${latitude})
+      ) RETURNING id, name, gender, age, ST_Y(location) as latitude, ST_X(location) as longitude`
+    );
+    return newHedgehog;
+  } catch (error) {
+    logger.error(error);
+  }
+}
+
 
 // TODO: Yksittäisen siilin hakeminen tietokannasta ID:llä
 
--- server/src/routes/hedgehog.ts
+++ server/src/routes/hedgehog.ts
@@ -1,5 +1,13 @@
-import { getAllHedgehogs } from "@server/application/hedgehog";
-import { FastifyInstance, FastifyPluginOptions } from "fastify";
+import { addHedgehog, getAllHedgehogs, getHedgehog } from "@server/application/hedgehog";
+import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
+import { Hedgehog } from "@shared/hedgehog";
+interface GetHedgehogRequest {
+  Params: {
+    id: string
+  }
+}
+
+
 
 export function hedgehogRouter(
   fastify: FastifyInstance,
@@ -13,6 +21,24 @@ export function hedgehogRouter(
       hedgehogs,
     });
   });
+
+  fastify.get("/:id", async function (_request: FastifyRequest<GetHedgehogRequest>, reply: FastifyReply) {
+    console.log(_request.params)
+    const id = parseInt(_request.params.id, 10)
+    const hedgehog = await getHedgehog(id);
+
+    return reply.code(200).send({
+      hedgehog,
+    });
+  });
+
+  fastify.post("/", async function (_request: FastifyRequest<{ Body: Hedgehog }>, reply) {
+    const { name, age, gender, latitude, longitude } = _request.body
+    const hedgehog = await addHedgehog(name, age, gender, latitude, longitude)
+    return reply.code(201).send({
+      hedgehog,
+    });
+  })
 
   // TODO: Yksittäisen siilin hakeminen tietokannasta ID:llä
   // fastify.get(...);
